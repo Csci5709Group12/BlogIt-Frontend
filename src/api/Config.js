c6330a13d82f140a4641b9ac60bbd686560b2e56
Csci5709Group12 <csci5709.group12@gmail.com>
2024-07-30 23:15:31 -0300

    Update Config.js

--- src/api/Config.js
+++ src/api/Config.js
@@ -1,2 +1,2 @@
-// export const SERVER_HOST = "https://csci5709-project-7umy.onrender.com";
-export const SERVER_HOST = "http://localhost:5000";
+export const SERVER_HOST = "https://blogit-backend-h2ww.onrender.com";
+// export const SERVER_HOST = "http://localhost:5000";
